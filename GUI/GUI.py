8fa2854abcc3463ee8dc8e3a6681d077916342f2
johncava <john.kevin.cava@gmail.com>
2016-01-23 13:35:04 -0700

    did some touched to the GUI

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -21,13 +21,21 @@ class ReaderDisplay(Canvas):
     NOTE_WIDTHS_PER_UPDATE = 0.5    # the number of note widths to shift all the notes every UPDATE_INTERVAL
     UPDATE_INTERVAL = 100 # milliseconds
     STAFF_OFFSET = 15 # pixels; the number of pixels between the top staff line and the top of the canvas
+    WIDTH = 1000
+    HEIGHT = 400
+    TOP_OFFSET = 130
+    LR_OFFSET = 0
     
     def __init__(self, parent):
-        Canvas.__init__(self, parent, bg="#FFFFFF")
+        Canvas.__init__(self, parent, bg="#FFFFFF", width = self.WIDTH, height = self.HEIGHT)
         self.notes = []
         self.render_staff()
-        
         self.after(ReaderDisplay.UPDATE_INTERVAL, self.update_staff)
+    
+    def create_lines(self,LR_margin, top_margin):
+        for i in xrange(5):
+            y = i * Note.NOTE_WIDTH + ReaderDisplay.STAFF_OFFSET
+            self.create_line(LR_margin, y + top_margin, self.WIDTH - LR_margin, y + top_margin)
         
     def render_staff(self):
         # TODO TEMP
@@ -35,17 +43,14 @@ class ReaderDisplay(Canvas):
         
         self.delete("all")
         
-        for i in xrange(5):
-            y = i * Note.NOTE_WIDTH + ReaderDisplay.STAFF_OFFSET
-            self.create_line(0, y, self['width'], y)
-        
+        self.create_lines(self.LR_OFFSET, self.TOP_OFFSET)
         # TODO TEMP
         if len(self.notes) == 0 or self.notes[len(self.notes) - 1].time % 3 == 0:
             self.notes.append(Note(Note.B3_VALUE, 0))
         
         for note in self.notes:
-            center_x = round(int(self['width']) - ((note.time + 0.5) * Note.NOTE_WIDTH * ReaderDisplay.NOTE_WIDTHS_PER_UPDATE))
-            center_y = (5 + note.value - Note.B3_VALUE)  * Note.NOTE_WIDTH / 2
+            center_x = round(int(self.WIDTH) - ((note.time + 0.5) * Note.NOTE_WIDTH * ReaderDisplay.NOTE_WIDTHS_PER_UPDATE))
+            center_y = self.TOP_OFFSET + (5 + note.value - Note.B3_VALUE)  * Note.NOTE_WIDTH / 2
             
             self.create_line(center_x - Note.NOTE_WIDTH / 2, center_y - Note.NOTE_WIDTH / 2, center_x + Note.NOTE_WIDTH / 2 + 1, center_y + Note.NOTE_WIDTH / 2 + 1)
             self.create_line(center_x - Note.NOTE_WIDTH / 2, center_y + Note.NOTE_WIDTH / 2, center_x + Note.NOTE_WIDTH / 2 + 1, center_y - Note.NOTE_WIDTH / 2 - 1)
@@ -67,14 +72,14 @@ class MainWindow(Frame):
         self.pack(fill=BOTH, expand=1)
         
         reader_display = ReaderDisplay(self)
-        reader_display.grid(columnspan=4)
+        reader_display.grid(columnspan=4, padx = 125, pady = 125)
         
         quit_button = Button(self, text="Quit", command=self.quit)
         quit_button.grid(row=3, column=5, rowspan=1, columnspan=1)
 
 def main():
     root = Tk()
-    root.geometry("800x800+250+50")
+    root.geometry("1000x400")
     app = MainWindow(root)
     root.mainloop()
 
